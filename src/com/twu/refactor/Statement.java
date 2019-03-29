0124c2a5bef379f52c224b4154f4bcf7e2292423
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-03-29 17:54:01 +0530

    refactored Statement class

--- src/com/twu/refactor/Statement.java
+++ src/com/twu/refactor/Statement.java
@@ -3,19 +3,23 @@ package com.twu.refactor;
 import java.util.List;
 
 class Statement {
-  static String generateStatement(String name, double totalAmount, int frequentRenterPoints, List<Rental> rentalList) {
-    StringBuilder result = new StringBuilder("Rental Record for " + name + "\n");
 
+  static String generateStatement(String name, double totalAmount, int frequentRenterPoints, List<Rental> rentalList) {
+    StringBuilder result = new StringBuilder(generateTitleLine(name));
     for (Rental rental : rentalList) {
       String movieName = rental.getMovieTitle();
       double movieRent = rental.getRentForEachMovie();
       result.append(appendEachMovieDetails(movieName, movieRent));
     }
-    result.append(appendFooterLines(totalAmount, frequentRenterPoints));
+    result.append(generateFooterLines(totalAmount, frequentRenterPoints));
     return result.toString();
   }
 
-  private static String appendFooterLines(double totalAmount, int frequentRenterPoints) {
+  private static String generateTitleLine(String name) {
+    return "Rental Record for " + name + "\n";
+  }
+
+  private static String generateFooterLines(double totalAmount, int frequentRenterPoints) {
     return "Amount owed is " + totalAmount + "\nYou earned " + frequentRenterPoints + " frequent renter points";
   }
 
